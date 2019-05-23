6e3ef67bdce49210437587653d30dc9fcd416c4c
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-05-22 23:55:58 -0500

    Added il2cpp invoker project

---
+++ Il2CppInvoker/Il2CppRunner.cs
@@ -0,0 +1,47 @@
+﻿using NiceIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Unity.IL2CPP;
+using Unity.IL2CPP.Common.Profiles;
+
+namespace Il2CppInvoker
+{
+    class Il2CppRunner
+    {
+        internal static void Run(string il2cppRoot, string[] searchDirs, string[] convertDirs)
+        {
+            var output = Path.Combine(Environment.CurrentDirectory, "output").ToNPath();
+            var data = Path.Combine(Environment.CurrentDirectory, "data").ToNPath();
+            var symbols = Path.Combine(Environment.CurrentDirectory, "symbols").ToNPath();
+            var execs = Path.Combine(Environment.CurrentDirectory, "execs").ToNPath();
+
+            var mono = Path.Combine(il2cppRoot, "..", "MonoBleedingEdge");
+            var etc = Path.Combine(mono, "etc").ToNPath();
+            var lib = Path.Combine(mono, "lib").ToNPath();
+
+            var monoSearch = Path.Combine(mono, "lib", "mono", "4.5").ToNPath();
+
+            CodeGenOptions.SetToDefaults();
+            CodeGenOptions.Dotnetprofile = Profile.Net45.Name;
+            CodeGenOptions.EmitMethodMap = true;
+            CodeGenOptions.EmitNullChecks = true;
+            CodeGenOptions.EmitSourceMapping = true;
+            CodeGenOptions.EnableArrayBoundsCheck = true;
+            CodeGenOptions.EnableDivideByZeroCheck = true;
+            CodeGenOptions.EnablePrimitiveValueTypeGenericSharing = true;
+            CodeGenOptions.EnableStacktrace = true;
+            CodeGenOptions.MonoRuntime = false;
+
+            NPath[] asms;
+            using (Globals.Use()) // needed for il2cpp to behave
+            {
+                asms = AssemblyConverter.ConvertAssemblies(convertDirs, new NPath[0], output, data, symbols, execs,
+                    lib, etc, searchDirs.Select(s => s.ToNPath()).Append(monoSearch).ToArray(), null, new NPath[0]).ToArray();
+            }
+        }
+    }
+}
---
+++ Il2CppInvoker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Il2CppInvoker
+{
+    class Program
+    {
+        private const string FilePathStoreFile = "il2cpp.path";
+        static void Main(string[] args)
+        {
+            var il2cppPath = "";
+            if (File.Exists(FilePathStoreFile))
+                il2cppPath = File.ReadAllText(FilePathStoreFile);
+            else
+            {
+                Console.WriteLine("Put the full path to your IL2CPP folder here (contains il2cpp_root an libil2cpp folder):");
+                Console.Write("> ");
+                il2cppPath = Console.ReadLine();
+                File.WriteAllText(FilePathStoreFile, il2cppPath);
+            }
+
+            var build = Path.Combine(il2cppPath, "build");
+            var buildAsDir = new DirectoryInfo(build);
+
+            foreach(var dll in buildAsDir.EnumerateFiles("*.dll"))
+                Assembly.LoadFrom(dll.FullName);
+
+            RunnerProxy(il2cppPath, new string[0], new[] { build });
+        }
+
+        private static void RunnerProxy(string il2cppRoot, string[] searchDirs, string[] convertDirs)
+        {
+            Il2CppRunner.Run(il2cppRoot, searchDirs, convertDirs);
+        }
+    }
+}
