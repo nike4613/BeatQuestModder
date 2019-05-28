7e028f8e3998509c121a0537a206d61b6378b74b
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-05-28 02:17:27 -0500

    Fixed buffer move semantics, and reduced some memory copying in really bad cases

--- Host/BeatQuestModder/include/buffer.h
+++ Host/BeatQuestModder/include/buffer.h
@@ -7,7 +7,7 @@
 #include <stdexcept>
 #include <functional>
 #include <type_traits>
-
+#include <utility>
 
 template<typename T, bool readwrite = true> class buffer;
 
@@ -28,9 +28,8 @@ class buffer<T, true>
 	constexpr buffer(std::nullptr_t) noexcept : memptr(nullptr), size_(0), ownAlloc(false), deallocator({}) {}
 	constexpr void adjust(size_t ptr_off, size_t size) noexcept { memptr += ptr_off; size_ = size; }
 public:
-	template<bool readwrite>
-	// copy actual buffer data
-	constexpr buffer(const buffer<T, readwrite>& other) noexcept : buffer(other.size()) { memcpy(memptr, other.data(), size_); }
+	// require a readonly to copy
+	constexpr buffer(const buffer<T, false>& other) noexcept : buffer(other.size()) { memcpy(memptr, other.data(), size_); }
 	// properly transfer ownership
 	constexpr buffer(const buffer<T, true>&& other) noexcept : buffer(other.memptr, other.size_, std::move(other.deallocator)) {
 		ownAlloc = other.ownAlloc; other.ownAlloc = false;
@@ -38,14 +37,26 @@ public:
 
 	buffer() noexcept : buffer(static_cast<size_t>(0)) {}
 	buffer(size_t count) noexcept : size_(count), ownAlloc(true) {
-		memptr = reinterpret_cast<T*>(malloc(size_ * sizeof(T)));
+		memptr = new T[size_];
 	}
 	constexpr buffer(T* ptr, size_t count, std::function<void(T*)> dealloc) noexcept : buffer(ptr, count, [dealloc](T* m, size_t s) { dealloc(m); }) {}
 	constexpr buffer(T* ptr, size_t count, dealloc_t dealloc) noexcept : buffer(ptr, count) { deallocator = dealloc; }
 	constexpr buffer(T* ptr, size_t count) noexcept : memptr(ptr), size_(count), ownAlloc(false) {}
 	~buffer() noexcept {
-		if (ownAlloc) free(memptr);
+		if (ownAlloc) delete memptr;
 		else if (deallocator) deallocator(memptr, size());
+	}
+
+	constexpr buffer<T, true>& operator=(const buffer<T, true>& other) {
+		std::swap(memptr, other.memptr); std::swap(size_, other.size_); std::swap(ownAlloc, other.ownAlloc);
+		std::swap(deallocator, other.deallocator);
+		return *this;
+	}
+
+	constexpr buffer<T, true>& operator=(buffer<T, true>&& other) {
+		memptr = other.memptr; size_ = other.size_; ownAlloc = other.ownAlloc; other.ownAlloc = false;
+		deallocator = std::move(other.deallocator);
+		return *this;
 	}
 
 	[[nodiscard]] constexpr auto is_writable() const noexcept { return true; }
@@ -54,9 +65,14 @@ public:
 	[[nodiscard]] constexpr auto size() const noexcept { return size_; }
 	[[nodiscard]] constexpr auto empty() const noexcept { return size() == 0; }
 
-	// does not do bounds checking
-	[[nodiscard]] constexpr auto slice(size_t start, size_t len) const noexcept { return buffer<T, true>(data() + start, len); }
+	// does not do bounds checking; gives a readonly view into this buffer
+	[[nodiscard]] constexpr auto slice(size_t start, size_t len) const noexcept { return buffer<T, false>(data() + start, len); }
 	[[nodiscard]] constexpr auto slice(size_t len) const noexcept { return slice(0, len); }
+
+	[[nodiscard]] constexpr auto as_readonly() const noexcept { return slice(size()); }
+
+	// implicit conversion avoids copying data
+	constexpr operator buffer<T, false>() const noexcept { return as_readonly(); }
 
 	[[nodiscard]] constexpr const T& operator[](size_t index) const noexcept { return memptr[index]; }
 	[[nodiscard]] constexpr T& operator[](size_t index) noexcept { return memptr[index]; }
@@ -88,7 +104,8 @@ class buffer<T, false>
 	buffer<T, true> rwBuffer;
 public:
 	// keep a hold of the other buffer
-	constexpr buffer(const buffer<T, true>& other) noexcept : rwBuffer(other), memptr(rwBuffer.data()), size_(rwBuffer.size()) {}
+	explicit constexpr buffer(const buffer<T, true>& other) noexcept : rwBuffer(other.as_readonly()) 
+		{ memptr = rwBuffer.data(); size_ = rwBuffer.size(); }
 	// no need to copy
 	constexpr buffer(const buffer<T, false>& other) noexcept : memptr(other.data()), size_(other.size()), rwBuffer(nullptr) {}
 	// no need to transfer ownership
@@ -98,9 +115,23 @@ public:
 	constexpr buffer(const T* ptr, size_t count) noexcept : memptr(ptr), size_(count), rwBuffer(nullptr) {}
 	~buffer() noexcept {}
 
+	constexpr buffer<T, false>& operator=(const buffer<T, false>& other) {
+		std::swap(memptr, other.memptr); std::swap(size_, other.size_);
+		std::swap(rwBuffer, other.rwBuffer);
+		return *this;
+	}
+	constexpr buffer<T, false>& operator=(const buffer<T, false>&& other) {
+		memptr = other.memptr; size_ = other.size_;
+		rwBuffer = std::move(other.rwBuffer);
+		return *this;
+	}
+
 	[[nodiscard]] constexpr auto is_writable() const noexcept { return false; }
 	template<typename U>
-	[[nodiscard]] constexpr auto reinterpret_as() const noexcept { return buffer<U, false>(reinterpret_cast<const U*>(memptr), size_); }
+	[[nodiscard]] constexpr auto reinterpret_as() const noexcept { 
+		static_assert(sizeof(T) == sizeof(U));
+		return buffer<U, false>(reinterpret_cast<const U*>(memptr), size_); 
+	}
 
 	[[nodiscard]] constexpr auto data() const noexcept { return memptr; }
 	[[nodiscard]] constexpr auto size() const noexcept { return size_; }
--- Host/BeatQuestModder/src/BeatQuestModder.cpp
+++ Host/BeatQuestModder/src/BeatQuestModder.cpp
@@ -12,13 +12,31 @@ auto code_buffer = make_buffer("\x55\x48\x8b\x05\xb8\x13\x00\x00").reinterpret_a
 
 int main()
 {
+	buffer<uint8_t> rw_buffer = code_buffer;
+
 	capstone::ARMHandle handle;
 	if (handle.Error() != CS_ERR_OK)
 	{
 		cerr << "Could not set up Capstone: " << handle.Error() << endl;
 		return -1;
 	}
-	auto instructions = handle.Disassemble(code_buffer, 0x1000);
+
+	auto instructions = handle.Disassemble(rw_buffer, 0x1000);
+	if (instructions.size() <= 0)
+	{
+		cerr << "Test code disassembled to nothing!" << endl;
+		return -2;
+	}
+
+	for (auto instr : instructions)
+	{
+		cout << "0x" << hex << right << setw(8) << setfill('0') << instr.address << resetiosflags(cout.flags())
+			<< ":\t" << instr.mnemonic << "\t    " << instr.op_str << endl;
+	}
+
+	rw_buffer[2] = 0x7b;
+
+	instructions = handle.Disassemble(rw_buffer, 0x1000);
 	if (instructions.size() <= 0)
 	{
 		cerr << "Test code disassembled to nothing!" << endl;
