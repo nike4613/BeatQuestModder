f1758a5cdf946589fa1a52157cb061a97dbdeb21
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-05-29 23:18:21 -0500

    Properly added LLVM to CMake (Takes ages to build, and currently fails trying to build second time)

--- Host/BeatQuestModder/src/BeatQuestModder.cpp
+++ Host/BeatQuestModder/src/BeatQuestModder.cpp
@@ -6,47 +6,80 @@
 #include "buffer.h"
 #include <iomanip>
 
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
 using namespace std;
 
-auto code_buffer = "\x55\x48\x8b\x05\xb8\x13\x00\x00"_buffer.reinterpret_as<uint8_t>();
+constexpr auto code_buffer = "\x55\x48\x8b\x05\xb8\x13\x00\x00"_buffer;
 
 int main()
 {
-	buffer<uint8_t> rw_buffer = code_buffer;
+	buffer<uint8_t> rw_buffer = code_buffer.reinterpret_as<uint8_t>();
 
-	capstone::ARMHandle handle;
-	if (handle.Error() != CS_ERR_OK)
 	{
-		cerr << "Could not set up Capstone: " << handle.Error() << endl;
-		return -1;
+		capstone::ARMHandle handle;
+		if (handle.Error() != CS_ERR_OK)
+		{
+			cerr << "Could not set up Capstone: " << handle.Error() << endl;
+			return -1;
+		}
+
+		auto instructions = handle.Disassemble(rw_buffer, 0x1000);
+		if (instructions.size() <= 0)
+
+
+		{
+			cerr << "Test code disassembled to nothing!" << endl;
+			return -2;
+		}
+
+		for (auto instr : instructions)
+		{
+			cout << "0x" << hex << right << setw(8) << setfill('0') << instr.address << resetiosflags(cout.flags())
+				<< ":\t" << instr.mnemonic << "\t    " << instr.op_str << endl;
+		}
+
+		rw_buffer[2] = 0x7b;
+
+		instructions = handle.Disassemble(rw_buffer, 0x1000);
+		if (instructions.size() <= 0)
+		{
+			cerr << "Test code disassembled to nothing!" << endl;
+			return -2;
+		}
+
+		for (auto instr : instructions)
+		{
+			cout << "0x" << hex << right << setw(8) << setfill('0') << instr.address << resetiosflags(cout.flags())
+				<< ":\t" << instr.mnemonic << "\t    " << instr.op_str << endl;
+		}
 	}
 
-	auto instructions = handle.Disassemble(rw_buffer, 0x1000);
-	if (instructions.size() <= 0)
 	{
-		cerr << "Test code disassembled to nothing!" << endl;
-		return -2;
-	}
+		llvm::LLVMContext context;
+		llvm::IRBuilder<> builder(context);
+		auto module = llvm::make_unique<llvm::Module>("LLVM BC Module", context);
 
-	for (auto instr : instructions)
-	{
-		cout << "0x" << hex << right << setw(8) << setfill('0') << instr.address << resetiosflags(cout.flags())
-			<< ":\t" << instr.mnemonic << "\t    " << instr.op_str << endl;
-	}
+		auto i32 = llvm::Type::getInt32Ty(context);
+		auto func = llvm::Function::Create(llvm::FunctionType::get(i32, false), llvm::Function::LinkageTypes::ExternalLinkage, "TestFunc", module.get());
+		func->setDoesNotRecurse();
+		auto fnBlock = llvm::BasicBlock::Create(context, "entry", func);
+		builder.SetInsertPoint(fnBlock);
 
-	rw_buffer[2] = 0x7b;
+		auto retConst = llvm::ConstantInt::get(i32, llvm::APInt(32, 15, true));
+		auto retInst = builder.CreateRet(retConst);
 
-	instructions = handle.Disassemble(rw_buffer, 0x1000);
-	if (instructions.size() <= 0)
-	{
-		cerr << "Test code disassembled to nothing!" << endl;
-		return -2;
-	}
+		llvm::verifyFunction(*func);
 
-	for (auto instr : instructions)
-	{
-		cout << "0x" << hex << right << setw(8) << setfill('0') << instr.address << resetiosflags(cout.flags())
-			<< ":\t" << instr.mnemonic << "\t    " << instr.op_str << endl;
+		module->print(llvm::outs(), nullptr);
 	}
 
 	cout << "Hello CMake." << endl;
